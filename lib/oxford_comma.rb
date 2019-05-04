f0b931e5773d8c3cddfece84b01e307da35a2475
Oana Berentan <oana.berentan@yahoo.com>
2019-05-04 14:02:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,6 @@
 def oxford_comma(array)
   return nil if array.nil?
-   return array[0] if array.length == 1
-   return array[0..-3].join(', ') + " and " + array[-1] if array.length > 1
+    if array.length == 1
+    return array[0]
+end
 end
