455e3721846c0f34c430d049dc04678d01bb9946
Oana Berentan <oana.berentan@yahoo.com>
2019-05-04 14:18:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
   if array.length == 1
     return array.join
-  if array.length == 2
+  elsif 
+     array.length == 2
     return array.join(" and ")
   end
-end
+  end
 end
