215ea588578dd9ccc779bb9032da8e441f094093
Oana Berentan <oana.berentan@yahoo.com>
2019-05-04 14:20:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,6 @@
 def oxford_comma(array)
-  if array.length == 1
+  if array.size == 1
     return array.join
-  elsif 
-     array.length == 2
+  elsif array.size == 2
     return array.join(" and ")
-  end
-  end
 end
